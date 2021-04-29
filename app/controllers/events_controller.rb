d73b129b5df2a5195ef85a107d5a2f9dfd90b4d7
emmy <emmy66418@gmail.com>
2021-04-29 19:17:05 +0200

    Add controller's file to make a logic

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,77 +2,45 @@ class EventsController < ApplicationController
   before_action :set_event, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, except: %i[ index show ]
 
-  # GET /events or /events.json
   def index
-    @events = Event.all.order('created_at DESC')
-    @event = Event.new
+    @events = Event.all
+    #@upcoming_events = Event.upcoming_events
+    #@past_events = Event.past_events
   end
 
-  # GET /events/1 or /events/1.json
   def show
+    @event = Event.find(params[:id])
+    @attendees = @event.attendees
   end
 
-  # GET /events/new
   def new
     @event = Event.new
   end
 
-  # GET /events/1/edit
-  def edit; end
+  def create
+    @event = current_user.created_events.build(event_params)
 
-  # POST /events or /events.json
-  # def create
-  #   @event = Event.new(event_params)
-
-  #   respond_to do |format|
-  #     if @event.save
-  #       format.html { redirect_to @event, notice: "Event was successfully created." }
-  #       format.json { render :show, status: :created, location: @event }
-  #     else
-  #       format.html { render :new, status: :unprocessable_entity }
-  #       format.json { render json: @event.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  def create 
-    @event = current_user.created_events.build(event_params) 
-    if 
-      @event.save redirect_to @event, notice: "Your event was created" 
-    else 
-      render :new 
-    end 
-  end 
-
-  # PATCH/PUT /events/1 or /events/1.json
-  def update
     respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to @event, notice: "Event was successfully updated." }
-        format.json { render :show, status: :ok, location: @event }
+      if @event.save
+        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.json { render :show, status: :created, location: @event }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @event.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /events/1 or /events/1.json
-  def destroy
-    @event.destroy
-    respond_to do |format|
-      format.html { redirect_to events_url, notice: "Event was successfully destroyed." }
-      format.json { head :no_content }
-    end
+  def show_upcoming
+    @upcoming_events = Event.upcoming_events
+  end
+
+  def show_past
+    @past_events = Event.past_events
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-  def set_event
-    @event = Event.find(params[:id])
-  end
 
-  # Only allow a list of trusted parameters through.
   def event_params
     params.require(:event).permit(:event)
   end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+      end
+    
+      def show
+        if signed_in?
+          @created_events = current_user.created_events
+          @attended_events = current_user.event_attended
+        else
+          redirect_to login_path
+        end
+      end
+    
+      def new
+        @user = User.new
+      end
+    
+      def create
+        @user = User.new(user_params)
+        redirect_to users_path if @user.save
+      end
+    
+      private
+    
+      def user_params
+        params.require(:user).permit(:name, :email, :password)
+      end
+end
