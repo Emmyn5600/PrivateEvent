f161b1fb6299b27927157a9309bbf808a5ee2027
emmy <emmy66418@gmail.com>
2021-05-04 00:08:40 +0200

    Modify the controler to save the inserted data

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -66,6 +66,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:event)
+      params.require(:event).permit(:title, :description, :location, :date)
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,11 @@
 class Event < ApplicationRecord
   belongs_to 	:creator, :class_name => "User"
 
-	has_many 	:event_attendees, :foreign_key => :attended_event_id
+  has_many 	:event_attendees, :foreign_key => :attended_event_id
   has_many 	:attendees, :through => :event_attendees
 
   scope :upcoming, -> { where("Date >= ?", Date.today).order('Date ASC') }
-	scope :past, 	 -> { where("Date <  ?", Date.today).order('Date DESC') }
+  scope :past, 	 -> { where("Date <  ?", Date.today).order('Date DESC') }
 
 	validates  :location, length: { minumum: 5, maximum: 100 }
 	validates  :description, length: { minumum: 5, maximum: 500 }
