612669fb9bb253778f6dda76473e9f0288eb359b
emmy <emmy66418@gmail.com>
2021-05-06 00:17:36 +0200

    Fix rubocop errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   def notice_flash
     out = ''
     if flash[:notice]
-      out += "<div class=\"notification is-primary global-notification\">"
+      out += '<div class="notification is-primary global-notification">'
       out += "<p class=\"notice\"> #{notice}</p>"
-      out += "</div>"
+      out += '</div>'
     end
     out.html_safe
   end
@@ -12,9 +12,9 @@ module ApplicationHelper
   def alert_flash
     out = ''
     if flash[:alert]
-      out += "<div class=\"notification is-danger global-notification\">"
+      out += '<div class="notification is-danger global-notification">'
       out += "<p class=\"alert\"> #{alert}</p>"
-      out += "</div>"
+      out += '</div>'
     end
     out.html_safe
   end
@@ -23,22 +23,22 @@ module ApplicationHelper
     out = ''
     if user_signed_in?
       out += link_to 'Create Event', new_event_url, class: 'button is-primary is-inverted'
-      out += "<p class=\"control\">"
+      out += '<p class="control">'
       out += link_to current_user.username, edit_user_registration_path, class: 'button is-primary'
-      out += "</p>"
-      out += "<p class=\"control\">"
-      out += link_to 'Your events', user_path(current_user), class:'button is-primary'
-      out += "</p>"
-      out += "<p>"
-      out += link_to 'Logout', destroy_user_session_path, method: :delete, class:'button is-primary'
-      out += "</p>"
-    else      
-      out += "<p class=\"control\">"
+      out += '</p>'
+      out += '<p class="control">'
+      out += link_to 'Your events', user_path(current_user), class: 'button is-primary'
+      out += '</p>'
+      out += '<p>'
+      out += link_to 'Logout', destroy_user_session_path, method: :delete, class: 'button is-primary'
+    else
+      out += '</p>'
+      out += '<p class="control">'
       out += link_to 'Sign In', new_user_session_path, class: 'button is-primary'
-      out += "</p>"
-      out += "<p class=\"control\">"
+      out += '</p>'
+      out += '<p class="control">'
       out += link_to 'Sign Up', new_user_registration_path, class: 'button is-primary'
-      out += "</p>"
+      out += '</p>'
     end
     out.html_safe
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :events, foreign_key: :creator_id, class_name: 'Event'
   has_many :event_attendees, foreign_key: :attendee_id
-    
+
   has_many :attended_events, through: :event_attendees
 
   validates :username, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
